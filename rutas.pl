e157bc1bea98146efa22cffb140fbaefec7c9493
Fernando Dodino <fernando.dodino@gmail.com>
2019-08-07 06:26:39 -0300

    Add tabling for performance enhancement

--- rutas.pl
+++ rutas.pl
@@ -7,6 +7,8 @@
   ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝ ╚═════╝╚═╝  ╚═╝                                             
 */
 
+% :- table kilometrosViaje/3.
+
 /** Casos base => la distancia directa es conocida */
 distancia(buenosAires, puertoMadryn, 1300).
 distancia(puertoMadryn, puertoDeseado, 732).
@@ -16,11 +18,13 @@ distancia(rioGallegos, calafate, 304).
 distancia(calafate, chalten, 213).
 
 kilometrosViaje(Origen, Destino, Kms):-
-    distancia(Origen, Destino, Kms).
+    distancia(Origen, Destino, Kms).  
 kilometrosViaje(Origen, Destino, KmsTotales):-
     distancia(Origen, PuntoIntermedio, KmsIntermedios),
     kilometrosViaje(PuntoIntermedio, Destino, KmsFinales),
     KmsTotales is KmsIntermedios + KmsFinales.
+kilometrosViaje(Destino, Origen, Kms):-
+    kilometrosViaje(Origen, Destino, Kms), !.  
 
 totalViaje(Origen, Destino, Kms):-kilometrosViaje(Origen, Destino, Kms).
 totalViaje(Destino, Origen, Kms):-kilometrosViaje(Origen, Destino, Kms).
